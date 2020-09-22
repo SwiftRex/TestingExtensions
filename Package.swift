546083037a31a0f505b3d1a2f9ccab67e996eeec
Thomas Mellenthin <github@dysternis.de>
2020-09-22 18:01:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "TestingExtensionsDynamic", type: .dynamic, targets: ["TestingExtensions"])
     ],
     dependencies: [
-        .package(name: "SnapshotTesting", url: "https://github.com/thedavidharris/swift-snapshot-testing.git", .upToNextMajor(from: "1.8.2")),
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .upToNextMajor(from: "1.8.2")),
         .package(name: "SwiftRex", url: "https://github.com/SwiftRex/SwiftRex.git", from: "0.7.1")
     ],
     targets: [
